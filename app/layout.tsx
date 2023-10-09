185863e473338bd335ff9e34c2d24d159bd8ef63
HuyDuong <93182239+FeikVizeege@users.noreply.github.com>
2023-10-09 16:39:32 +0700

    UserProvider.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Sidebar from '@/components/Sidebar'
 import SupabaseProvider from '@/providers/SupabaseProvider'
+import UserProvider from '@/providers/UserProvider'
 import type { Metadata } from 'next'
 import { Figtree } from 'next/font/google'
 
@@ -20,9 +21,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <SupabaseProvider>
-          <Sidebar>
-            {children}
-          </Sidebar>
+          <UserProvider>
+            <Sidebar>
+              {children}
+            </Sidebar>
+          </UserProvider>
         </SupabaseProvider>
       </body>
     </html>
