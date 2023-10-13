61cf371fde42b53de9dcf6c729328c4d8259bcc0
HuyDuong <93182239+FeikVizeege@users.noreply.github.com>
2023-10-13 15:00:29 +0700

    PageContent & SongItem components

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,9 +1,13 @@
+import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
 
 export const revalidate = 0;
 
-export default function Home() {
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <div className="
       bg-neutral-900
@@ -44,7 +48,7 @@ export default function Home() {
           </h1>
         </div>
         <div>
-          List of Songs!
+          <PageContent songs={songs} />
         </div>
       </div>
     </div>
